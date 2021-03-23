6a6ed24577e0b39c364c719b38acf58cc0e25abc
Your Name <you@example.com>
2021-03-23 16:57:19 -0300

    subindo novos arquivos

---
+++ quiz/base/views.py
@@ -0,0 +1,17 @@
+
+from django.shortcuts import render
+
+# Create your views here.
+from quiz.base.models import Pergunta
+
+
+def home (requisicao):
+    return  render(requisicao, 'base/home.html')
+
+def perguntas (requisicao, indice):
+    pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice -1]
+    contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
+    return  render(requisicao, 'base/game.html', context=contexto)
+
+def classificacao (requisicao):
+    return render(requisicao,'base/classificacao.html')
